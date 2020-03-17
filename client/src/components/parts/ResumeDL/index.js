b1506d3a357a069966a49534e01012d6aec91d6e
k-fizzle <foscat4o1k@outlook.com>
2020-03-17 09:43:26 -0500

    spellcheck download

--- client/src/components/parts/ResumeDL/index.js
+++ client/src/components/parts/ResumeDL/index.js
@@ -7,7 +7,7 @@ const ResumeDL = props => {
         <div className={props.className} style={{justifyContent: "space-between",...props.style}}>
 
             <div className="col-6">
-                <a href="./downloads/Kyle_Foser_Résumé.pdf" download="Kyle_Foser_Résumé.pdf">
+                <a href="./downloads/Kyle_Foster_Résumé.pdf" download="Kyle_Foster_Résumé.pdf">
                     <Button style={{boxShadow: shadows.div2}} >
                         <img src="./images/PDF_DL.png" alt="download pdf" />
                     </Button>
@@ -15,7 +15,7 @@ const ResumeDL = props => {
             </div>
 
             <div className="col-6">
-                <a href="./downloads/Kyle_Foser_Résumé.docx" download="Kyle_Foser_Résumé.docx">
+                <a href="./downloads/Kyle_Foster_Résumé.docx" download="Kyle_Foster_Résumé.docx">
                     <Button style={{boxShadow: shadows.div2}} >
                         <img src="./images/Word_DL.png" alt="download pdf" />
                     </Button>
